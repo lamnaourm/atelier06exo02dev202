82f158cf65f65c027cf72099b024636c5019748a
lamnaourm <mohammed.lamnaour@gmail.com>
2022-10-06 13:53:00 +0100

    first commit

--- src/components/Calcul.jsx
+++ src/components/Calcul.jsx
@@ -5,30 +5,15 @@ class Calcul extends Component {
         super(props)
 
         this.state = {
-           distance: 0,
-           poids: 0,
-           mode: 'normal',
-           cout:0
+          
         }
     }
 
     handleOnChange = (evt) => {
-        this.setState({[evt.target.name] : evt.target.value});
     }
 
     handleOnSubmit = (evt) => {
-        evt.preventDefault();
-        let c = 0;
         
-        if(this.state.poids<=10)
-            c = this.state.distance * 0.5;
-        else 
-            c = this.state.distance * (this.state.poids/10) * 0.3;
-
-        if(this.state.mode === 'express')
-            c += c * 0.2
-        
-        this.setState({cout: c})
 
     }
     
@@ -38,22 +23,22 @@ class Calcul extends Component {
                 <form onSubmit={this.handleOnSubmit}>
                     <div>
                         <label>Distance : </label>
-                        <input type='number' name='distance' value={this.state.distance} onChange={this.handleOnChange}/>
+                        <input type='number' name='distance' />
                     </div>
                     <div>
                         <label>Poids : </label>
-                        <input type='number' name='poids' value={this.state.poids} onChange={this.handleOnChange}/>
+                        <input type='number' name='poids' />
                     </div>
                     <div>
                         <label>Mode de transport :</label>
-                        <input type='radio' name='mode' value='express' checked={this.state.mode==='express'} onChange={this.handleOnChange}/> Express 
-                        <input type='radio' name='mode' value='normal' checked={this.state.mode==='normal'} onChange={this.handleOnChange}/> Normal
+                        <input type='radio' name='mode' value='express' /> Express 
+                        <input type='radio' name='mode' value='normal' /> Normal
                     </div>
                     <hr/>
                     <input type='submit' value='Calculer'/>
                     <div>
                         <label>Cout total :</label>
-                        <input type='number' name='cout' value={this.state.cout} disabled/>
+                        <input type='number' name='cout' disabled/>
                     </div>
                 </form>
             </div>
